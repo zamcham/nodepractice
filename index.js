a6fca8989d12c165992d2991a288cc764b6beba1
Cristian Zambrano <criszamcham@gmail.com>
2024-03-09 09:50:59 -0700

    Test JOI library

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -21,6 +24,25 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.post('/api/courses', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 
