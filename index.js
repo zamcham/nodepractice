e511d7d494313efe31d2771b8e9f3d4fef5fc355
Cristian Zambrano <criszamcham@gmail.com>
2024-03-09 09:25:47 -0700

    Test ports

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
 });
