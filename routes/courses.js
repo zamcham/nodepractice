bd53d89830567605daa1b946786286d1f8118c76
Cristian Zambrano <criszamcham@gmail.com>
2024-03-09 14:54:15 -0700

    Practice express structuring

---
+++ routes/courses.js
@@ -0,0 +1,61 @@
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
+router.get('/', (req, res) => {
+    res.send(courses);
+});
+
+router.get('/:id', (req, res) => {
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+});
+
+router.post('/', (req, res) => {
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/:id', (req, res) => {
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
+
+module.exports = router;
