9462cd026d9ef00c43767207f53ca9a8c128417b
Cristian Zambrano <criszamcham@gmail.com>
2024-03-10 15:15:46 -0700

    Work on 'vidly' project

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-const { get } = require('config');
 const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const express = require('express');
+const app = express();
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
-const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean
-});
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
-// Classes, objects
-const Course = mongoose.model('Course', courseSchema);
-
-async function createCourse() {
-    const course = new Course({
-        name: 'Angular Course',
-        author: 'Cristian',
-        tags: ['angular', 'frontend'],
-        isPublished: true
-    });
-
-    const result = await course.save();
-    console.log(result);
-}
-
-async function getCourses() {
-    const courses = await Course
-        .find({ author: 'Cristian', isPublished: true })
-        .limit(10)
-        .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    console.log(courses);
-}
-
-getCourses();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
