a6d896daf8a4c9691a1d8b490863024f7d261e29
Cristian Zambrano <criszamcham@gmail.com>
2024-03-13 10:53:10 -0700

    Add async middleware

---
+++ middleware/async.js
@@ -0,0 +1,10 @@
+// Where 'handler' is a function that takes a request and response object and returns a promise.
+module.export = function asyncMiddleware(handler) {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res);
+        } catch (error) {
+            next(error);
+        }
+    };
+}
--- routes/genres.js
+++ routes/genres.js
@@ -4,15 +4,12 @@ const { Genre, validate } = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const asyncMiddleware = require('../middleware/async');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/', asyncMiddleware(async (req, res, next) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+}));
 
 router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body);
