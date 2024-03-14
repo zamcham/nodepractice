41cc8caf8561ee37a07c969e7cfe073d11656a21
Cristian Zambrano <criszamcham@gmail.com>
2024-03-14 12:58:13 -0700

    Update separation of concerns

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const config = require('config');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
 require('./startup/routes')(app);
 require('./startup/db')();
-
-
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
